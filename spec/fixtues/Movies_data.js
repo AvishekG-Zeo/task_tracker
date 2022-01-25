906175c5e16f5f8f5fd8b4f3a65b1264f6813876
AvishekG-Zeo <94888657+AvishekG-Zeo@users.noreply.github.com>
2022-01-25 17:18:32 +0530

    Add files via upload

---
+++ spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"Golmaal",
+        rating:"4.5",
+        genres:"comedy",
+        comment:"very Good"
+    },
+    movie2:{
+        name:"spider man no way home",
+        rating:"4.8",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"Tenet",
+        rating:"4.3",
+        genres:"action",
+        comment:"Good"
+    },
+    movie4:{
+        name:"Stree",
+        rating:"4.5",
+        genres:"horror",
+        comment:"Good"
+    },
+}
